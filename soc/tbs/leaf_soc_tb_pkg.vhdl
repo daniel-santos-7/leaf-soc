643061a28b7c0b0c12e94734c6f2d1a22197ac8d
Daniel Santos <danielsantos7@outlook.com.br>
2026-02-18 21:29:13 -0300

    Update SoC testbench

---
+++ soc/tbs/leaf_soc_tb_pkg.vhdl
@@ -0,0 +1,189 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+package leaf_soc_tb_pkg is
+
+    constant CLK_PERIOD : time := 20 ns;
+
+    constant MEM_SIZE : natural := 4 * 1024;
+
+    type byte_array is array (0 to MEM_SIZE-1) of std_logic_vector(7 downto 0);
+
+    impure function load_mem_data(constant file_path : string) return byte_array;
+
+    procedure uart_tx(
+        constant baud : natural;
+        constant data : std_logic_vector(7 downto 0);
+        signal tx : out std_logic
+    );
+
+    procedure uart_rx(
+        constant baud : natural;
+        signal data : out std_logic_vector(7 downto 0);
+        signal rx : in std_logic
+    );
+
+    component xip_ctrl is
+        port (
+            clk   : in  std_logic;
+            rst   : in  std_logic;
+            cyc_i : in  std_logic;
+            stb_i : in  std_logic;
+            we_i  : in  std_logic;
+            sel_i : in  std_logic_vector(3  downto 0);
+            adr_i : in  std_logic_vector(31 downto 0);
+            dat_i : in  std_logic_vector(31 downto 0);
+            ack_o : out std_logic;
+            err_o : out std_logic;
+            dat_o : out std_logic_vector(31 downto 0)
+        );
+    end component xip_ctrl;
+
+    -- constant BITS       : natural := 24;
+    -- constant MEM_SIZE   : natural := 2**BITS;
+    -- constant UART_BAUD  : natural := 434;
+    -- constant WM_CMD     : std_logic_vector(7 downto 0) := x"31";
+
+    -- ld_program: process
+    --     variable byte  : byte_type;
+    --     variable addr  : integer;
+    --     variable size  : std_logic_vector(31 downto 0);
+    --     variable frame : std_logic_vector(9 downto 0);
+    -- begin
+    --     rx <= '1';
+    --     wait for 25*CLK_PERIOD;
+
+    --     file_open(bin, PROGRAM);
+    --     addr := 0;
+    --     while not endfile(bin) loop
+    --         read(bin, byte);
+    --         mem(addr) := std_logic_vector(to_unsigned(byte_type'pos(byte), 8));
+    --         addr := addr + 1;
+    --     end loop;
+    --     file_close(bin);
+
+    --     frame := '1' & WM_CMD & '0';
+    --     for i in 0 to 9 loop
+    --         rx <= frame(i);
+    --         wait for UART_BAUD*CLK_PERIOD;
+    --     end loop;
+
+    --     size := std_logic_vector(to_unsigned(addr, 32));
+
+    --     frame := '1' & size(31 downto 24) & '0';
+    --     for i in 0 to 9 loop
+    --         rx <= frame(i);
+    --         wait for UART_BAUD*CLK_PERIOD;
+    --     end loop;
+
+    --     frame := '1' & size(23 downto 16) & '0';
+    --     for i in 0 to 9 loop
+    --         rx <= frame(i);
+    --         wait for UART_BAUD*CLK_PERIOD;
+    --     end loop;
+
+    --     frame := '1' & size(15 downto  8) & '0';
+    --     for i in 0 to 9 loop
+    --         rx <= frame(i);
+    --         wait for UART_BAUD*CLK_PERIOD;
+    --     end loop;
+
+    --     frame := '1' & size(7  downto  0) & '0';
+    --     for i in 0 to 9 loop
+    --         rx <= frame(i);
+    --         wait for UART_BAUD*CLK_PERIOD;
+    --     end loop;
+
+    --     for i in 0 to addr loop
+    --         frame := '1' & mem(i) & '0';
+    --         for j in 0 to 9 loop
+    --             rx <= frame(j);
+    --             wait for UART_BAUD*CLK_PERIOD;
+    --         end loop;
+    --         report integer'image(i);
+    --     end loop;
+
+    --     rx <= '1';
+    --     wait;
+    -- end process ld_program;
+
+    -- wr_output: process
+    --     variable data : std_logic_vector(7 downto 0);
+    --     type charfile is file of character;
+    --     file out_file: charfile;
+    -- begin
+    --     data := x"FF";
+    --     file_open(out_file, "STD_OUTPUT", write_mode);
+    --     while true loop
+    --         wait until tx = '0';
+    --         wait for UART_BAUD*CLK_PERIOD;
+    --         wait for UART_BAUD*CLK_PERIOD/2;
+    --         for i in 0 to 7 loop
+    --             data(i) := tx;
+    --             wait for UART_BAUD*CLK_PERIOD;
+    --         end loop;
+    --         write(out_file, character'val(to_integer(unsigned(data))));
+    --     end loop;
+    --     file_close(out_file);
+    --     wait;
+    -- end process wr_output;
+
+end package leaf_soc_tb_pkg;
+
+package body leaf_soc_tb_pkg is
+
+    impure function load_mem_data(constant file_path : string) return byte_array is
+        type char_file is file of character;
+        file mem_file : char_file;
+        variable byte : character;
+        variable mem_data : byte_array := (others => (others => '0'));
+        variable addr : natural := 0;
+    begin
+        file_open(mem_file, file_path, read_mode);
+        while not endfile(mem_file) loop
+            if addr < MEM_SIZE then
+                read(mem_file, byte);
+                mem_data(addr) := std_logic_vector(to_unsigned(character'pos(byte), 8));
+                addr := addr + 1;
+            else
+                report "load_mem_data: program too large for memory" severity error;
+                exit;
+            end if;
+        end loop;
+        file_close(mem_file);
+        return mem_data;
+    end function load_mem_data;
+
+    procedure uart_tx(
+        constant baud : natural;
+        constant data : std_logic_vector(7 downto 0);
+        signal tx : out std_logic
+    ) is
+    begin
+        tx <= '0';
+        wait for baud*CLK_PERIOD;
+        for i in 7 downto 0 loop
+            tx <= data(i);
+            wait for baud*CLK_PERIOD;
+        end loop;
+        tx <= '1';
+        wait for baud*CLK_PERIOD;
+    end procedure uart_tx;
+
+    procedure uart_rx(
+        constant baud : natural;
+        signal data : out std_logic_vector(7 downto 0);
+        signal rx : in std_logic
+    ) is
+    begin
+        wait until rx = '0';
+        wait for baud*CLK_PERIOD/2;
+        for i in 7 downto 0 loop
+            wait for baud*CLK_PERIOD;
+            data(i) <= rx;
+        end loop;
+        wait for baud*CLK_PERIOD;
+    end procedure uart_rx;
+
+end package body leaf_soc_tb_pkg;
---
+++ soc/tbs/xip_ctrl.vhdl
@@ -0,0 +1,88 @@
+library IEEE;
+library work;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use work.leaf_soc_tb_pkg.all;
+
+entity xip_ctrl is
+    port (
+        clk   : in  std_logic;
+        rst   : in  std_logic;
+        cyc_i : in  std_logic;
+        stb_i : in  std_logic;
+        we_i  : in  std_logic;
+        sel_i : in  std_logic_vector(3  downto 0);
+        adr_i : in  std_logic_vector(31 downto 0);
+        dat_i : in  std_logic_vector(31 downto 0);
+        ack_o : out std_logic;
+        err_o : out std_logic;
+        dat_o : out std_logic_vector(31 downto 0)
+    );
+end entity xip_ctrl;
+
+architecture rtl of xip_ctrl is
+
+    signal ack_reg : std_logic;
+
+    signal addr : natural;
+
+begin
+
+    addr <= to_integer(unsigned(adr_i(31 downto 2)));
+
+    process(clk, rst)
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                ack_reg <= '0';
+            else
+                ack_reg <= cyc_i and stb_i and not ack_reg;
+            end if;
+        end if;
+    end process;
+
+    process(clk, rst)
+
+        variable mem : byte_array;
+
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                mem := (others => (others => '0'));
+                dat_o <= (others => '0');
+            elsif cyc_i = '1' and stb_i = '1' and ack_reg = '0' then
+                if we_i = '1' then
+                    if sel_i(0) = '1' then
+                        mem(addr + 0) := dat_i(7 downto 0);
+                    end if;
+                    if sel_i(1) = '1' then
+                        mem(addr + 1) := dat_i(15 downto 8);
+                    end if;
+                    if sel_i(2) = '1' then
+                        mem(addr + 2) := dat_i(23 downto 16);
+                    end if;
+                    if sel_i(3) = '1' then
+                        mem(addr + 3) := dat_i(31 downto 24);
+                    end if;
+                else
+                    if sel_i(0) = '1' then
+                        dat_o(7 downto 0) <= mem(addr + 0);
+                    end if;
+                    if sel_i(1) = '1' then
+                        dat_o(15 downto 8) <= mem(addr + 1);
+                    end if;
+                    if sel_i(2) = '1' then
+                        dat_o(23 downto 16) <= mem(addr + 2);
+                    end if;
+                    if sel_i(3) = '1' then
+                        dat_o(31 downto 24) <= mem(addr + 3);
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    ack_o <= ack_reg;
+    err_o <= '0';
+
+end architecture rtl;
