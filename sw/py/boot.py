513c489db496f25fde2bac0662545286ba40f6d7
Daniel Santos <danielsantos7@outlook.com.br>
2026-02-26 14:24:10 -0300

    Update boot.py

--- sw/py/boot.py
+++ sw/py/boot.py
@@ -7,20 +7,20 @@ package boot_pkg is
 
     constant BOOT_SIZE : natural := {boot_size};
 
-    type byte_array is array (0 to BOOT_SIZE-1) of std_logic_vector(7 downto 0);
+    type byte_array is array (natural range <>) of std_logic_vector(7 downto 0);
 
-    constant BOOT_DATA : byte_array := ({boot_data}\n\t);
+    constant BOOT_DATA : byte_array(0 to BOOT_SIZE-1) := ({boot_data}\n\t);
 
 end package boot_pkg;'''
 
 if __name__ == '__main__':
     argv_len = len(sys.argv)
-    if argv_len == 1:
-        print("Usage: python boot.py")
+    if argv_len < 2:
+        print("Usage: python boot.py <bin_file>")
         exit(1)
     bin_file = sys.argv[1]
     with open(bin_file, "rb") as f:
         data = f.read()
-        boot_data = ",".join([f'\n\t{i} => x"{b:02X}"' for i, b in enumerate(data)])
+        boot_data = ",".join([f'\n\t\t{i} => x"{b:02X}"' for i, b in enumerate(data)])
         package = PACKAGE_TEMPLATE.format(boot_size=len(data), boot_data=boot_data)
         print(package)
