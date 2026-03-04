2513a7525b1504534ba600ecc2a7584dd2101c87
Daniel Santos <danielsantos7@outlook.com.br>
2026-03-04 00:01:29 -0300

    Add process to print UART received data

--- soc/tbs/leaf_soc_tb.vhdl
+++ soc/tbs/leaf_soc_tb.vhdl
@@ -19,6 +19,8 @@ architecture tb of leaf_soc_tb is
     signal rx  : std_logic;
     signal tx  : std_logic;
 
+
+
     signal uart_data : std_logic_vector(7 downto 0);
 
     signal clk_en : std_logic := '0';
@@ -35,12 +37,20 @@ begin
     clk <= not clk after (CLK_PERIOD/2) when clk_en = '1' else '0';
 
     uart_rx_proc: process
+        type char_file is file of character;
+        file out_file : char_file;
         variable rx_data : std_logic_vector(7 downto 0);
+        variable char : character;
     begin
+        file_open(out_file, "STD_OUTPUT", write_mode);
         rx_loop : loop
             uart_receive(tx, rx_data);
             uart_data <= rx_data;
+            char := character'val(to_integer(unsigned(rx_data)));
+            write(out_file, char);
         end loop;
+        file_close(out_file);
+        wait;
     end process uart_rx_proc;
 
     test: process
@@ -58,6 +68,8 @@ begin
         end loop;
 
         leaf_soc_send_program(rx, uart_data, PROGRAM);
+
+        wait for 1 ms;
 
         wait until rising_edge(clk);
         clk_en <= '0';
