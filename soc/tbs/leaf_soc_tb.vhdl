3993374f5bc226d6f46495be2a06dce0975a9f0d
Daniel Santos <danielsantos7@outlook.com.br>
2026-04-27 21:51:24 -0300

    refactor: comment out uart_tx_proc for clarity in testbench

--- soc/tbs/leaf_soc_tb.vhdl
+++ soc/tbs/leaf_soc_tb.vhdl
@@ -52,14 +52,30 @@ begin
         wait;
     end process uart_rx_proc;
 
+    -- uart_tx_proc: process
+    --     type char_file is file of character;
+    --     file in_file : char_file;
+    --     variable tx_data : std_logic_vector(7 downto 0);
+    --     variable char : character;
+    -- begin
+    --     file_open(in_file, "STD_INPUT", read_mode);
+    --     while not endfile(in_file) loop
+    --         read(in_file, char);
+    --         tx_data := std_logic_vector(to_unsigned(character'pos(char), 8));
+    --         uart_transmit(rx, tx_data);
+    --     end loop;
+    --     file_close(in_file);
+    --     wait;
+    -- end process uart_tx_proc;
+
     test: process
     begin
-        rst  <= '1';
+        rst  <= '0';
         rx   <= '1';
         clk_en <= '1';
         wait until rising_edge(clk);
 
-        rst <= '0';
+        rst <= '1';
         wait until rising_edge(clk);
 
         for i in 0 to 511 loop
