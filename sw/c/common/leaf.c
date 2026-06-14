cbeec7dee54eca1733dc66d1a86a3b5b413a8423
Daniel Santos <danielsantos7@outlook.com.br>
2026-06-14 13:15:12 -0300

    Implement UART functionality in leaf.c and leaf.h, including initialization, data transmission, and reception methods; add Makefile for build automation.

--- sw/c/common/leaf.c
+++ sw/c/common/leaf.c
@@ -1,99 +1,172 @@
 #include "leaf.h"
 
-char volatile *iostatus = (char *) 0x0;
-char volatile *iodata = (char *) 0xC;
+#define SOC_CLOCK_HZ 50000000
 
-void int2string(int num, char *str) {
+static volatile uint8_t *const uart_stat = (volatile uint8_t *)(UART_BASE + UART_STAT);
+static volatile uint8_t *const uart_data = (volatile uint8_t *)(UART_BASE + UART_DATA);
+static volatile uint32_t *const uart_brdv = (volatile uint32_t *)(UART_BASE + UART_BRDV);
+
+void uart_init(uint16_t baud_div)
+{
+    *uart_brdv = baud_div;
+}
+
+void uart_putchar(char c)
+{
+    while ((*uart_stat & UART_STAT_TX_READY) != UART_STAT_TX_READY);
+    *uart_data = (uint8_t)c;
+}
+
+char uart_getchar(void)
+{
+    while ((*uart_stat & UART_STAT_RX_READY) != UART_STAT_RX_READY);
+    return (char)(*uart_data);
+}
+
+void uart_puts(const char *str)
+{
+    while (*str)
+        uart_putchar(*str++);
+}
+
+void uart_gets(char *str, int max_len)
+{
     int i = 0;
-    int isNegative = 0;
-    char tempStr[20]; // Tamanho máximo da string
+    char c;
+    while (i < max_len - 1)
+    {
+        c = uart_getchar();
+        if (c == '\n' || c == '\r')
+            break;
+        str[i++] = c;
+    }
+    str[i] = '\0';
+}
 
-    // Verifica se o número é negativo
-    if (num < 0) {
-        isNegative = 1;
+void uart_send_string(char *str)
+{
+    uart_puts(str);
+}
+
+void uart_receive_string(char *str)
+{
+    uart_gets(str, 256);
+}
+
+void uart_send_integer(int num)
+{
+    char str[20];
+    itoa(num, str);
+    uart_puts(str);
+}
+
+int uart_receive_integer(void)
+{
+    char str[20];
+    uart_gets(str, sizeof(str));
+    return atoi(str);
+}
+
+int uart_write(const char *buf, int len)
+{
+    for (int i = 0; i < len; i++)
+        uart_putchar(buf[i]);
+    return len;
+}
+
+uint64_t get_cycle(void)
+{
+    uint32_t lo, hi;
+    __asm__ volatile("csrr %0, cycle\n\t"
+                     "csrr %1, cycleh"
+                     : "=r"(lo), "=r"(hi));
+    return ((uint64_t)hi << 32) | lo;
+}
+
+void delay_cycles(uint32_t cycles)
+{
+    uint32_t start;
+    __asm__ volatile("csrr %0, cycle" : "=r"(start));
+    while (1)
+    {
+        uint32_t now;
+        __asm__ volatile("csrr %0, cycle" : "=r"(now));
+        if (now - start >= cycles)
+            break;
+    }
+}
+
+void delay_us(uint32_t us)
+{
+    delay_cycles(us * (SOC_CLOCK_HZ / 1000000));
+}
+
+void itoa(int num, char *str)
+{
+    int i = 0;
+    int is_neg = 0;
+    char tmp[20];
+
+    if (num < 0)
+    {
+        is_neg = 1;
         num = -num;
     }
 
-    // Lida com o caso especial de 0
-    if (num == 0) {
+    if (num == 0)
+    {
         str[i++] = '0';
         str[i] = '\0';
         return;
     }
 
-    // Converte cada dígito em um caractere
-    while (num != 0) {
-        int digit = num % 10;
-        tempStr[i++] = digit + '0';
-        num = num / 10;
+    while (num != 0)
+    {
+        tmp[i++] = (num % 10) + '0';
+        num /= 10;
     }
 
-    // Adiciona o sinal de negativo, se necessário
-    if (isNegative) {
-        tempStr[i++] = '-';
-    }
+    if (is_neg)
+        tmp[i++] = '-';
 
-    // Inverte a string
-    int j;
-    for (j = 0; j < i; j++) {
-        str[j] = tempStr[i - 1 - j];
-    }
-    str[i] = '\0'; // Adiciona o caractere nulo para terminar a string
+    for (int j = 0; j < i; j++)
+        str[j] = tmp[i - 1 - j];
+    str[i] = '\0';
 }
 
-int string2int(char *str) {
-    int resultado = 0; // Variável para armazenar o resultado da conversão
-    int sinal = 1; // Variável para armazenar o sinal do número (1 para positivo, -1 para negativo)
-    int i = 0; // Variável de iteração para percorrer a string
+int atoi(const char *str)
+{
+    int result = 0;
+    int sign = 1;
 
-    // Ignora espaços em branco iniciais
-    while (str[i] == ' ') {
-        i++;
+    while (*str == ' ')
+        str++;
+
+    if (*str == '-')
+    {
+        sign = -1;
+        str++;
+    }
+    else if (*str == '+')
+    {
+        str++;
     }
 
-    // Verifica o sinal do número
-    if (str[i] == '-') {
-        sinal = -1;
-        i++;
-    } else if (str[i] == '+') {
-        i++;
+    while (*str >= '0' && *str <= '9')
+    {
+        result = result * 10 + (*str - '0');
+        str++;
     }
 
-    // Percorre a string e acumula o valor numérico
-    while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9') {
-        resultado = resultado * 10 + (str[i] - '0');
-        i++;
-    }
-
-    return resultado * sinal;
+    return result * sign;
 }
 
-void uart_receive_string(char *str) {
-  for (int i = 0; str[i] != '\0'; i++) {  
-    while((*iostatus & 0x4) != 0x4);
-    str[i] = *iodata;
-    if (str[i] == '\n') {
-        str[i] = '\0';
-        break;
-    }
-  }
+void int2string(int num, char *str)
+{
+    itoa(num, str);
 }
 
-void uart_send_string(char *str) {
-  for(int i = 0; str[i] != '\0'; i++) {
-    while((*iostatus & 0x20) != 0x20);
-    *iodata = str[i];
-  }
-}
-
-void uart_send_integer(int num) {
-    char str[20];
-    int2string(num, str);
-    uart_send_string(str);
-}
-
-int uart_receive_integer() {
-    char str[20];
-    uart_receive_string(str);
-    return string2int(str);
+int string2int(char *str)
+{
+    return atoi(str);
 }
